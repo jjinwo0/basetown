124fb94cf2ceb6d87c963a927fc495e89e65d5fd
jinwoo <zvzv9808@gmail.com>
2023-01-28 21:50:42 +0900

    feat: Create MemberRepository find member Query

--- src/main/java/jinwoo/basetown/repository/MemberRepository.java
+++ src/main/java/jinwoo/basetown/repository/MemberRepository.java
@@ -1,9 +1,30 @@
 package jinwoo.basetown.repository;
 
+import jinwoo.basetown.dto.MemberDto;
 import jinwoo.basetown.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+    @Query("select m from Member m where m.username = :username")
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<Member> findUsernameList();
+
+    @Query("select new jinwoo.basetown.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username = :names")
+    List<Member> findByNames(@Param("names")Collection<String> names);
 
 
 }
